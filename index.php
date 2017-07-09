e77b4575e392bb6baa87a9d2ab755b96c3a55f51
ApoorvaJaiswal <jais.appy19960407@gmail.com>
2017-07-09 09:22:19 +0530

    almost done

--- index.php
+++ index.php
@@ -5,20 +5,9 @@
 
 
 </head>
-<body onload="init();">
+<body>
 
-<h1>Take a snapshot of the current video stream</h1>
-   Click on the Start WebCam button.
-     <p>
-    <button onclick="startWebcam();">Start WebCam</button>
-    <button onclick="stopWebcam();">Stop WebCam</button> 
-       <button onclick="snapshot();">Take Snapshot</button> 
-    </p>
-    <video onclick="snapshot(this);" width=400 height=400 id="video" controls autoplay></video>
-  <p>
-
-        Screenshots : <p>
-      <canvas  id="myCanvas" width="400" height="350"></canvas> 
+<video autoplay></video>
 
 <input type="button" id="b1" value="Write" onclick=writeUserData(1,"Apoorva","abc@gmail.com");></input>
 <input type="button" id="b2" value="Read" onclick=readUserData();></input>
@@ -31,61 +20,6 @@
 
 <script src="https://www.gstatic.com/firebasejs/4.1.3/firebase.js"></script>
 <script>
- navigator.getUserMedia = ( navigator.getUserMedia ||
-                             navigator.webkitGetUserMedia ||
-                             navigator.mozGetUserMedia ||
-                             navigator.msGetUserMedia);
-
-      var video;
-      var webcamStream;
-
-      function startWebcam() {
-        if (navigator.getUserMedia) {
-           navigator.getUserMedia (
-
-              // constraints
-              {
-                 video: true,
-                 audio: false
-              },
-
-              // successCallback
-              function(localMediaStream) {
-                  video = document.querySelector('video');
-                 video.src = window.URL.createObjectURL(localMediaStream);
-                 webcamStream = localMediaStream;
-              },
-
-              // errorCallback
-              function(err) {
-                 console.log("The following error occured: " + err);
-              }
-           );
-        } else {
-           console.log("getUserMedia not supported");
-        }  
-      }
-
-      function stopWebcam() {
-          webcamStream.stop();
-      }
-      //---------------------
-      // TAKE A SNAPSHOT CODE
-      //---------------------
-      var canvas, ctx;
-
-      function init() {
-        // Get the canvas and obtain a context for
-        // drawing in it
-        canvas = document.getElementById("myCanvas");
-        ctx = canvas.getContext('2d');
-      }
-
-      function snapshot() {
-         // Draws current image from the video element into the canvas
-        ctx.drawImage(video, 0,0, canvas.width, canvas.height);
-      }
-
 	function abc()
 	{
 		//var a=document.getElementById("fileToUpload");
